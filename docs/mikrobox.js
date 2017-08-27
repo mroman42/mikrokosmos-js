3766a0a8d640043839d94b859e616cd6cb375f6a
M42 <mromang08@gmail.com>
2017-08-28 00:12:02 +0200

    Mikrobox

--- docs/mikrobox.js
+++ docs/mikrobox.js
@@ -3,24 +3,25 @@ function textAreaAdjust(o) {
   o.style.height = (o.scrollHeight)+"px";
 }
 
-
 document.querySelectorAll(".mikrojs-console").forEach( function(container) {
     code = container.getElementsByTagName("script")[0].innerHTML;
     
     container.innerHTML =
-	'<textarea onkeyup="textAreaAdjust(this)" cols="80" style="overflow:hidden">' + code + '</textarea>' +
+	'<textarea style="height:auto;">' + code + '</textarea>' +
 	'<br>' +
 	'<button>execute</button>' +
 	'<br>' +
-	'<textarea cols="80" style="overflow:hidden" cols="80" readonly="true" id="outputarea"></textarea>'
+	'<textarea cols="80" style="overflow:hidden;resize:none" cols="80" readonly="true" id="outputarea"></textarea>'
 
     var inputarea = container.getElementsByTagName("textarea")[0];
     var outputarea = container.getElementsByTagName("textarea")[1];
     var button = container.getElementsByTagName("button")[0];
 
+    var editor = CodeMirror.fromTextArea(inputarea, {lineNumbers: true});
+    
     button.onclick = function () {
+	editor.save();
 	outputarea.value = mikrokosmos(inputarea.value).mkroutput;
 	textAreaAdjust(outputarea);
     }
 });
-
